daa1245d970f2ed76265ad95296cd14ef92d1fe7
Paulo Clavijo Esteban <paucls@gmail.com>
2017-11-13 08:20:30 +0000

    No need for autowire on a service with a single constructor

--- src/main/kotlin/com/paucls/runbookDDD/application/runbook/RunbookApplicationService.kt
+++ src/main/kotlin/com/paucls/runbookDDD/application/runbook/RunbookApplicationService.kt
@@ -3,13 +3,10 @@ package com.paucls.runbookDDD.application.runbook
 import com.paucls.runbookDDD.domain.model.runbook.RunbookAggregate
 import com.paucls.runbookDDD.persistence.RunbookRepository
 import com.paucls.runbookDDD.persistence.nextIdentity
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
-class RunbookApplicationService @Autowired constructor(
-        var runbookRepository: RunbookRepository
-) {
+class RunbookApplicationService(val runbookRepository: RunbookRepository) {
 
     fun createRunbook(c: CreateRunbookCommand): String {
         val runbookId = runbookRepository.nextIdentity()
